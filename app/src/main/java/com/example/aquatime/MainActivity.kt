c36b00c70393072b2437a39aed27f4870ef0bc27
Alexmaiden40 <blacksabbaths2xxx@gmail.com>
2024-07-19 03:03:26 -0300

    Hello World

---
+++ app/src/main/java/com/example/aquatime/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aquatime
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aquatime.R
+import com.example.aquatime.SegundaTela
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
